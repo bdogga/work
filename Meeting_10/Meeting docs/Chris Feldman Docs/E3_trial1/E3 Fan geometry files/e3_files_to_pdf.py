f7431ebfe9a9e6f3936535ed7c133cd0aad2534f
bharadwaj <bdogga.admis@gmail.com>
2020-08-01 01:54:45 -0400

    Finish code for plotting E3 files from Russel

--- Meeting_10/Meeting docs/Chris Feldman Docs/E3_trial1/E3 Fan geometry files/e3_files_to_pdf.py
+++ Meeting_10/Meeting docs/Chris Feldman Docs/E3_trial1/E3 Fan geometry files/e3_files_to_pdf.py
@@ -13,7 +13,7 @@ import sys
 # Plot (m', \theta) sections
 # Stored in the same PDF
 with PdfPages('NASA_plot_uv_coordinates.pdf') as pdf:
-   for i_sec in range(1, 20):
+   for i_sec in range(1, 21):
       print(i_sec)
       def add_page(i_sec):   
             # Read section files
@@ -41,10 +41,43 @@ with PdfPages('NASA_plot_uv_coordinates.pdf') as pdf:
          plt.ylabel(r'$v$',   fontsize = 16)
          plt.title(r"($u, v$) plot for section %d"%int(i_sec), fontsize = 16)
          plt.axis('equal')
-         print(i_sec)
-      add_page(i_sec)
+         # Save PDF
+         pdf.savefig()   
 
-   # Save PDF
-   pdf.savefig()
+      def add_page_short_ones(i_sec):   
+            # Read section files
+         filename            = 'fan-curve_layer' + str(i_sec) + '.dat'
+         f                   = open(filename, "r")
+         lines               = f.readlines()
+         lines1              = lines[105:193]     # Specify the rows where the coordinates exist
+         f.close()
+         # numpy arrays for storing blade section coordinates read from the file
+         u             = np.zeros(len(lines1))
+         v             = np.zeros(len(lines1))
+
+         # Store the blade coordinates read from the file in the arrays defined above
+         i                   = 0
+         for line in lines1:
+            u[i]      = float(line.split()[0])
+            v[i]      = float(line.split()[1])
+            i         = i + 1
+            
+         # Plot the 2D blade section
+         plt.figure()
+         plt.plot(u, v, 'k')
+         plt.xlabel(r"$u$", fontsize = 16)
+         plt.ylabel(r'$v$',   fontsize = 16)
+         plt.title(r"($u, v$) plot for section %d"%int(i_sec), fontsize = 16)
+         plt.axis('equal')
+
+         # Save PDF
+         pdf.savefig()      
+         
+      if int(i_sec) in range(5, 8):
+         add_page_short_ones(i_sec)
+      else:
+         add_page(i_sec)
+
+
 
 plt.close()
